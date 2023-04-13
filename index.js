384f3e43e3059a9f626e811d3f9ee3ad756b3953
Atai Nail <81722547+atainail@users.noreply.github.com>
2023-04-13 16:04:23 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 let add = document.querySelector(".button");
 let toDoList = document.querySelector("i");
+let input = document.querySelector(".form-control");
+
+input.value = " ";
 
 add.addEventListener('click', function(e) {
-    let input = document.querySelector(".form-control");
+    
     let today = new Date();
     let date = (today.getMonth() + 1) + "-" + today.getDate() + '-' +today.getFullYear();
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     let dateTime = date + " " + time;
+    let errorMessage = document.querySelector(".error");
 
-    let full = 
-    `<div class="toDo">      
-         <i class="bi bi-dash-circle"> ${input.value} | added on ${dateTime}</i> 
-     </div>`;
+    if(input.value == " "){
+        errorMessage.innerText = `Cannot be left blank`;  
+    } else {
+        errorMessage.innerText = " ";  
+        let full = 
+            `<div class="toDo">      
+                <i class="bi bi-dash-circle"> ${input.value} | added on ${dateTime}</i> 
+            </div>`;
 
-    let complete = document.createElement("p");
-    complete.innerHTML = full;
+        let complete = document.createElement("p");
+        complete.innerHTML = full;
 
-    toDoList.appendChild(complete);  
-    input.value = " ";
-    
+        toDoList.appendChild(complete);  
+        input.value = " ";
+    }  
 });
 
 toDoList.addEventListener('click', function (e){
